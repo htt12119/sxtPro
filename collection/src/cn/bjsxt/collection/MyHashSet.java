9a00eed76e0ae955e51ba0029ea863e2dfb78f3b
Houtingting <771377434@qq.com>
2017-08-31 23:51:50 +0800

    set

---
+++ collection/src/cn/bjsxt/collection/MyHashSet.java
@@ -0,0 +1,35 @@
+package cn.bjsxt.collection;
+
+import java.util.HashMap;
+
+/**
+ * 自定义自己的Set
+ * @author houtingting
+ *
+ */
+public class MyHashSet {
+	HashMap map;
+	private static final Object PRESENT = new Object();
+	
+	public MyHashSet(){
+		map = new HashMap();
+	}
+	
+	public int size(){
+		return map.size();
+	}
+	public void add(Object o){
+		map.put(o, PRESENT);//set的不可重复就是利用了Map里面键对象的不可重复！
+	}
+	
+	
+	public static void main(String[] args) {
+		MyHashSet s = new MyHashSet();
+		s.add("aaa");
+		System.out.println(s.size());
+		
+		
+	
+	}
+
+}
